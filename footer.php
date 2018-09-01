f452968a58d5415b22d5bfc95c3095899823c7e2
Gigih suryadi <40031458+GigihSuryadi@users.noreply.github.com>
2018-09-01 14:18:29 +0700

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,8 @@
+<!-- membuat footer -->
+<footer>
+  <p>&copy; 2018, Teknik Informatika Pelita Bangsa</p>
+</footer>
+</div>
+
+</body>
+</html>
---
+++ sidebar.php
@@ -0,0 +1,14 @@
+<!-- membuat sidebar -->
+<div class="sidebar">
+  <div class="widget-box">
+      <h3 class="title">Menu</h3>
+    <p>Artikel adalah karangan faktual secara lengkap dengan panjang tertentu yang dibuat untuk dipublikasikan (melalui koran, majalah, buletin, dsb) dan bertujuan menyampaikan gagasan dan fakta yang dapat meyakinkan, mendidik, dan menghibur.</p>
+
+
+  </div>
+  <!-- <div class="widget-box">
+      <h3 class="title">Widget Text</h3>
+      <p>Vestibulum lorem elit, iaculis in nisl volutpat, malesuada tincidunt arcu. Proin in leo fringilla, vestibulum mi porta, faucibus felis. Integer pharetra est nunc, nec pretium nunc pretium ac.</p>
+  </div> -->
+
+</div>
